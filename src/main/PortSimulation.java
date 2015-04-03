30dfa68cb2300dcf141f8b1926db54f3dab191a6
Chris Collander <cmcollander@archlinux.info>
2015-04-03 14:06:29 -0500

    work on portSimulation, including adding Xform.java from Oracle

--- src/main/PortSimulation.java
+++ src/main/PortSimulation.java
@@ -6,6 +6,7 @@
 package main;
 
 import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -16,6 +17,17 @@ import javafx.stage.Stage;
  */
 public class PortSimulation {
     private Map map;
+    final Group root = new Group();
+    final Xform world = new Xform();
+    final PerspectiveCamera camera = new PerspectiveCamera(true);
+    final Xform cameraXform = new Xform();
+    final Xform cameraXform2 = new Xform();
+    final Xform cameraXform3 = new Xform();
+    private static final double CAMERA_INITIAL_DISTANCE = -450;
+    private static final double CAMERA_INITIAL_X_ANGLE = 70.0;
+    private static final double CAMERA_INITIAL_Y_ANGLE = 320.0;
+    private static final double CAMERA_NEAR_CLIP = 0.1;
+    private static final double CAMERA_FAR_CLIP = 10000.0;
     
     public PortSimulation(Map map) {
         this.map = map;
@@ -28,5 +40,19 @@ public class PortSimulation {
         stage.setScene(new Scene(root,800,600,Color.BLACK));
         stage.setMaximized(true);
         stage.show();
+    }
+    
+    private void buildCamera() {
+        root.getChildren().add(cameraXform);
+        cameraXform.getChildren().add(cameraXform2);
+        cameraXform2.getChildren().add(cameraXform3);
+        cameraXform3.getChildren().add(camera);
+        cameraXform3.setRotateZ(180.0);
+ 
+        camera.setNearClip(CAMERA_NEAR_CLIP);
+        camera.setFarClip(CAMERA_FAR_CLIP);
+        camera.setTranslateZ(CAMERA_INITIAL_DISTANCE);
+        cameraXform.ry.setAngle(CAMERA_INITIAL_Y_ANGLE);
+        cameraXform.rx.setAngle(CAMERA_INITIAL_X_ANGLE);
     }
 }
