22bb32eb1b09bdd602c226f72b1e4085e566a278
Chris Collander <cmcollander@archlinux.info>
2015-04-03 16:22:23 -0500

    3d improvements

--- src/main/PortSimulation.java
+++ src/main/PortSimulation.java
@@ -9,7 +9,9 @@ import javafx.scene.Group;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
 import javafx.stage.Stage;
+import javafx.scene.shape.Box;
 
 /**
  *
@@ -25,11 +27,13 @@ public class PortSimulation {
     final Xform cameraXform = new Xform();
     final Xform cameraXform2 = new Xform();
     final Xform cameraXform3 = new Xform();
+    final Xform axisGroup = new Xform();
     private static final double CAMERA_INITIAL_DISTANCE = -450;
     private static final double CAMERA_INITIAL_X_ANGLE = 70.0;
     private static final double CAMERA_INITIAL_Y_ANGLE = 320.0;
     private static final double CAMERA_NEAR_CLIP = 0.1;
     private static final double CAMERA_FAR_CLIP = 10000.0;
+    private static final double AXIS_LENGTH = 250.0;
     
     public PortSimulation(Map map) {
         this.map = map;
@@ -37,14 +41,16 @@ public class PortSimulation {
     
     public void run() {
         root = new Group();
+        root.getChildren().add(world);
         stage = new Stage();
         scene = new Scene(root,800,600,Color.BLACK);
+        buildCamera();
+        buildAxes();
         stage.setTitle("3D Port Simulation");
         stage.setScene(scene);
-        stage.setMaximized(true);
-        stage.show();
-        buildCamera();
+        //stage.setMaximized(true);
         scene.setCamera(camera);
+        stage.show();
     }
     
     private void buildCamera() {
@@ -59,5 +65,31 @@ public class PortSimulation {
         camera.setTranslateZ(CAMERA_INITIAL_DISTANCE);
         cameraXform.ry.setAngle(CAMERA_INITIAL_Y_ANGLE);
         cameraXform.rx.setAngle(CAMERA_INITIAL_X_ANGLE);
+    }
+    
+    private void buildAxes() {
+        final PhongMaterial redMaterial = new PhongMaterial();
+        redMaterial.setDiffuseColor(Color.DARKRED);
+        redMaterial.setSpecularColor(Color.RED);
+ 
+        final PhongMaterial greenMaterial = new PhongMaterial();
+        greenMaterial.setDiffuseColor(Color.DARKGREEN);
+        greenMaterial.setSpecularColor(Color.GREEN);
+ 
+        final PhongMaterial blueMaterial = new PhongMaterial();
+        blueMaterial.setDiffuseColor(Color.DARKBLUE);
+        blueMaterial.setSpecularColor(Color.BLUE);
+ 
+        final Box xAxis = new Box(AXIS_LENGTH, 1, 1);
+        final Box yAxis = new Box(1, AXIS_LENGTH, 1);
+        final Box zAxis = new Box(1, 1, AXIS_LENGTH);
+        
+        xAxis.setMaterial(redMaterial);
+        yAxis.setMaterial(greenMaterial);
+        zAxis.setMaterial(blueMaterial);
+ 
+        axisGroup.getChildren().addAll(xAxis, yAxis, zAxis);
+        axisGroup.setVisible(true);
+        world.getChildren().addAll(axisGroup);
     }
 }
