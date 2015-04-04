126b6bcee2aac0e3a41da140b84b0631c3dce20e
Chris Collander <cmcollander@archlinux.info>
2015-04-03 20:18:10 -0500

    Ocean layer built and camera controls implemented through the mouse

--- src/main/PortSimulation.java
+++ src/main/PortSimulation.java
@@ -1,39 +1,57 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ Chris Collander
+ Abdul Rafey Khan
+ Clint Wetzel
+
+ CSE 1325-002
+ Semester Project
  */
 package main;
 
+import javafx.event.EventHandler;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.stage.Stage;
 import javafx.scene.shape.Box;
 
-/**
- *
- * @author Chris
- */
 public class PortSimulation {
     private Map map;
     private Stage stage;
     private Scene scene;
     private Group root;
-    final Xform world = new Xform();
     final PerspectiveCamera camera = new PerspectiveCamera(true);
+    
+    final Xform world = new Xform();
     final Xform cameraXform = new Xform();
     final Xform cameraXform2 = new Xform();
     final Xform cameraXform3 = new Xform();
+    final Xform oceanGroup = new Xform();
     final Xform axisGroup = new Xform();
+    
     private static final double CAMERA_INITIAL_DISTANCE = -450;
     private static final double CAMERA_INITIAL_X_ANGLE = 70.0;
     private static final double CAMERA_INITIAL_Y_ANGLE = 320.0;
     private static final double CAMERA_NEAR_CLIP = 0.1;
     private static final double CAMERA_FAR_CLIP = 10000.0;
-    private static final double AXIS_LENGTH = 250.0;
+    private static final double AXIS_LENGTH = 500;
+    
+    private static final double CONTROL_MULTIPLIER = 0.1;
+    private static final double SHIFT_MULTIPLIER = 10.0;
+    private static final double MOUSE_SPEED = 0.1;
+    private static final double ROTATION_SPEED = 2.0;
+    private static final double TRACK_SPEED = 0.3;
+    
+    double mousePosX;
+    double mousePosY;
+    double mouseOldX;
+    double mouseOldY;
+    double mouseDeltaX;
+    double mouseDeltaY;
     
     public PortSimulation(Map map) {
         this.map = map;
@@ -44,13 +62,91 @@ public class PortSimulation {
         root.getChildren().add(world);
         stage = new Stage();
         scene = new Scene(root,800,600,Color.BLACK);
+        scene.setFill(Color.SKYBLUE);
+        
+        handleMouse(scene, world);
+        
         buildCamera();
         buildAxes();
+        buildOcean();
+        
+        
         stage.setTitle("3D Port Simulation");
         stage.setScene(scene);
-        //stage.setMaximized(true);
-        scene.setCamera(camera);
+        stage.setMaximized(true);
         stage.show();
+        
+        scene.setCamera(camera);
+    }
+    
+    private void buildAxes() {
+        System.out.println("buildAxes()");
+        final PhongMaterial redMaterial = new PhongMaterial();
+        redMaterial.setDiffuseColor(Color.DARKRED);
+        redMaterial.setSpecularColor(Color.RED);
+
+        final PhongMaterial greenMaterial = new PhongMaterial();
+        greenMaterial.setDiffuseColor(Color.DARKGREEN);
+        greenMaterial.setSpecularColor(Color.GREEN);
+
+        final PhongMaterial blueMaterial = new PhongMaterial();
+        blueMaterial.setDiffuseColor(Color.DARKBLUE);
+        blueMaterial.setSpecularColor(Color.BLUE);
+
+        final Box xAxis = new Box(AXIS_LENGTH, 1, 1);
+        final Box yAxis = new Box(1, AXIS_LENGTH, 1);
+        final Box zAxis = new Box(1, 1, AXIS_LENGTH);
+
+        xAxis.setMaterial(redMaterial);
+        yAxis.setMaterial(greenMaterial);
+        zAxis.setMaterial(blueMaterial);
+
+        axisGroup.getChildren().addAll(xAxis, yAxis, zAxis);
+        axisGroup.setVisible(false);
+        world.getChildren().addAll(axisGroup);
+    }
+
+    private void handleMouse(Scene scene, final Node root) {
+        scene.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent me) {
+                mousePosX = me.getSceneX();
+                mousePosY = me.getSceneY();
+                mouseOldX = me.getSceneX();
+                mouseOldY = me.getSceneY();
+            }
+        });
+        scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent me) {
+                mouseOldX = mousePosX;
+                mouseOldY = mousePosY;
+                mousePosX = me.getSceneX();
+                mousePosY = me.getSceneY();
+                mouseDeltaX = (mousePosX - mouseOldX); 
+                mouseDeltaY = (mousePosY - mouseOldY); 
+                
+                double modifier = 1.0;
+                
+                if (me.isControlDown()) {
+                    modifier = CONTROL_MULTIPLIER;
+                } 
+                if (me.isShiftDown()) {
+                    modifier = SHIFT_MULTIPLIER;
+                }     
+                if (me.isPrimaryButtonDown()) {
+                    cameraXform.ry.setAngle(cameraXform.ry.getAngle() - mouseDeltaX*MOUSE_SPEED*modifier*ROTATION_SPEED);  
+                    cameraXform.rx.setAngle(cameraXform.rx.getAngle() + mouseDeltaY*MOUSE_SPEED*modifier*ROTATION_SPEED);  
+                }
+                else if (me.isSecondaryButtonDown()) {
+                    double z = camera.getTranslateZ();
+                    double newZ = z + mouseDeltaX*MOUSE_SPEED*modifier;
+                    camera.setTranslateZ(newZ);
+                }
+                else if (me.isMiddleButtonDown()) {
+                    cameraXform2.t.setX(cameraXform2.t.getX() + mouseDeltaX*MOUSE_SPEED*modifier*TRACK_SPEED);  
+                    cameraXform2.t.setY(cameraXform2.t.getY() + mouseDeltaY*MOUSE_SPEED*modifier*TRACK_SPEED);  
+                }
+            }
+        });
     }
     
     private void buildCamera() {
@@ -67,29 +163,17 @@ public class PortSimulation {
         cameraXform.rx.setAngle(CAMERA_INITIAL_X_ANGLE);
     }
     
-    private void buildAxes() {
-        final PhongMaterial redMaterial = new PhongMaterial();
-        redMaterial.setDiffuseColor(Color.DARKRED);
-        redMaterial.setSpecularColor(Color.RED);
- 
-        final PhongMaterial greenMaterial = new PhongMaterial();
-        greenMaterial.setDiffuseColor(Color.DARKGREEN);
-        greenMaterial.setSpecularColor(Color.GREEN);
- 
-        final PhongMaterial blueMaterial = new PhongMaterial();
-        blueMaterial.setDiffuseColor(Color.DARKBLUE);
-        blueMaterial.setSpecularColor(Color.BLUE);
- 
-        final Box xAxis = new Box(AXIS_LENGTH, 1, 1);
-        final Box yAxis = new Box(1, AXIS_LENGTH, 1);
-        final Box zAxis = new Box(1, 1, AXIS_LENGTH);
+    private void buildOcean() {
+        final PhongMaterial oceanMaterial = new PhongMaterial();
+        oceanMaterial.setDiffuseColor(Color.BLUE);
+        oceanMaterial.setSpecularColor(Color.LIGHTBLUE);
         
-        xAxis.setMaterial(redMaterial);
-        yAxis.setMaterial(greenMaterial);
-        zAxis.setMaterial(blueMaterial);
- 
-        axisGroup.getChildren().addAll(xAxis, yAxis, zAxis);
-        axisGroup.setVisible(true);
-        world.getChildren().addAll(axisGroup);
+        final Box ocean = new Box(530, 1, 350);
+        
+        ocean.setMaterial(oceanMaterial);
+        
+        oceanGroup.getChildren().add(ocean);
+        oceanGroup.setVisible(true);
+        world.getChildren().add(ocean);
     }
 }
